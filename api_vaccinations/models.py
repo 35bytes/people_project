1ce4857f7f2e3819c953597968150f0be3426e23
Karl Behrens <karlbehrensg@gmail.com>
2020-08-29 23:53:52 -0400

    Create Drug model

--- api_vaccinations/models.py
+++ api_vaccinations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Drug(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.CharField(max_length=255)
