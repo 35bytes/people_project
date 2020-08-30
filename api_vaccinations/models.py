0ba30982a0f808826d59271825c705e30c0e5d36
Karl Behrens <karlbehrensg@gmail.com>
2020-08-30 00:28:06 -0400

    Vaccination model

--- api_vaccinations/models.py
+++ api_vaccinations/models.py
@@ -5,3 +5,10 @@ class Drug(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=10, unique=True)
     description = models.CharField(max_length=255)
+
+
+class Vaccination(models.Model):
+    rut = models.CharField(max_length=10)
+    dose = models.FloatField()
+    date = models.DateField()
+    drug = models.ForeignKey(Drug, related_name='drug', on_delete=models.CASCADE)
