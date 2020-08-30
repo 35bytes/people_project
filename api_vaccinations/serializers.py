865e7dd428600468c159e97969e89b955b340cbb
Karl Behrens <karlbehrensg@gmail.com>
2020-08-30 00:29:05 -0400

    Vaccination serializer

--- api_vaccinations/serializers.py
+++ api_vaccinations/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Drug
+from .models import Drug, Vaccination
 
 
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
+        fields = '__all__'
+
+
+class VaccinationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vaccination
         fields = '__all__'
